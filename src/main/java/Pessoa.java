d266e6c12c46018a54780695e3a60d90665fab6d
felipekennedyalt <felipekennedyfvs2019@hotmail.com>
2020-06-20 12:44:11 -0300

    adicionando idade a pessoa

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -15,6 +15,17 @@ public class Pessoa {
     private String nome;
     private String cpf;
     private String email;
+    private int idade;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
 
     public String getEmail() {
         return email;
